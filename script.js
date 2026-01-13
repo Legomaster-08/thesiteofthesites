deb23c7f288351cb17ea7741f450d90764733408
mr.chitarra <111301390+Legomaster-08@users.noreply.github.com>
2026-01-13 11:23:49 +0100

    Updated index and added contact list and navbar

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var url = "images/site1.jpg";
+  document.body.style.backgroundImage = "linear-gradient(rgba(10,10,10,0.6), rgba(10,10,10,0.6)), url('" + url + "')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundColor = "#0a0a0a";
+});
